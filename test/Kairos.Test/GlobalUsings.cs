f252b425851c62ab35c67a0a77623aa9496cc2cc
Adyllson <domingosady11son@gmail.com>
2025-04-10 19:35:50 +0100

    terminando de testar o respositorio do tipo de eventos

--- test/Kairos.Test/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-#region </Test>
-    global using Xunit;
-#endregion
-
-#region </Domain>
-    global using Kairos.Domain.Entities;
-    global using Kairos.Domain.Abstractions.Validation;
-#endregion
